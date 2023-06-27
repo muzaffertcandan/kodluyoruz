8c3fae6b663c946b08c8714356db14b7f4e48689
Muzaffer Talha Candan <muzaffertcandan@gmail.com>
2023-06-28 01:59:53 +0300

    Ders 5 e kadar

---
+++ jsDersleri/js/03-degisken-kurallari.js
@@ -0,0 +1,31 @@
+//javascript Camel case kullanır yani
+
+let fullName = "muzaffer talha candan"
+
+//js küçük büyük harf duyarlıdır
+
+let serverPassword = "1234" //bu doğru
+
+let serverPASsword = "1234" //bu kullanım yanlış ve farklı bir değişken tanımlamış oluyoruz
+
+// uppercase kullanımı
+
+const PASSWORD = "345" // doğru kullanım budur 
+
+const password = "123" // bu kullanım yanlıştır
+
+const SERVER_PASSWORD = "sifre" //doğru kullanım budur
+
+//değişkenlerde türkçe ve diğer dillerin kullanımı
+
+// let türkçeBilgi = "bilgiler" // yanlış kullanım
+let turkceBilgi = "bilgiler bazı bişiler bişi" //doğru kullanım
+
+// anlamsız isimlendirmeler kullanmayın örn: x y z i e k
+let x = 123 // yanlış
+
+// boolean tanımlanımı
+
+let isActive = true
+let hasPassword = false
+//tek satırdaki karakter sayısı 80 i geçmemeli (eski ekranlarda sıkıntı)
