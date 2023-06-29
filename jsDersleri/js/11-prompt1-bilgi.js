81dc84a81b11bfad08b311293aa59130062fdfcb
muzaffertcandan <muzaffertcandan@gmail.com>
2023-06-29 17:56:00 +0300

    11 bitti

--- jsDersleri/js/11-prompt1-bilgi.js
+++ jsDersleri/js/11-prompt1-bilgi.js
@@ -1,2 +1,6 @@
 
-let fullName = prompt
+let fullName = prompt("lütfen adınızı giriniz")
+
+let greeting = document.querySelector("#greeting")
+console.log(fullName);
+greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
