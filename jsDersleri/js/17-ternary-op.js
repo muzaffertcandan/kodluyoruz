e86c5e3f42a45f5fd83304ad4bdd936da42f3b1a
Muzaffer Talha Candan <muzaffertcandan@gmail.com>
2023-07-01 01:10:26 +0300

    sayı notunu harf notuna çeviren program yapıldı

--- jsDersleri/js/17-ternary-op.js
+++ jsDersleri/js/17-ternary-op.js
@@ -3,4 +3,16 @@ let info = document.querySelector("#info")
 
 //ternary kullanımı
 // kosul ? dogruysa : yanlissa 
-info.innerHTML = `${userName.length > 0 ? userName:"kullanıcı bilginiz bulunamadı."} `
+
+// info.innerHTML = `${userName.length > 0 ? userName:"kullanıcı bilginiz bulunamadı."} `
+// bu daha doğru kullanım
+info.innerHTML = `${userName ? userName:"kullanıcı bilginiz bulunamadı."} `
+
+let boolean;
+
+const isBooleanTrue = boolean ? true : false;
+
+console.log(isBooleanTrue)
+>> false
+
+console.log(boolean)
