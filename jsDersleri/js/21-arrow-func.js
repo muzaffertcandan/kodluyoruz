5e309613208c9b1f5cb247292b313795d0277ae8
Muzaffer Talha Candan <muzaffertcandan@gmail.com>
2023-07-02 16:59:48 +0300

    vs

--- jsDersleri/js/21-arrow-func.js
+++ jsDersleri/js/21-arrow-func.js
@@ -3,6 +3,6 @@ function hello(firstName) {
     console.log(`Merhaba ${firstName}`);
 }
 
-const helloFuncV1 = (firstName) => {} 
+const helloFuncV1 = (firstName) => { console.log(`Merhaba ${firstName}`)} 
 
 hello("javascript")
