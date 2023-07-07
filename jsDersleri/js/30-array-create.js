13979bfe14ab13bd3a6fc2557467ea4635bc94dd
Muzaffer Talha Candan <muzaffertcandan@gmail.com>
2023-07-07 17:08:06 +0300

    30

---
+++ jsDersleri/js/30-array-create.js
@@ -0,0 +1,13 @@
+// dizilerle çalışmak
+
+let DOMAIN = "muzafferrc.com";
+let isActive = false;
+let items = [1, 2, 3, isActive, DOMAIN];
+
+
+let emptyArray = []
+
+//array eleman sayısı öğrenmek
+console.log(items.length);
+
+document.querySelector("#info").innerHTML = items
