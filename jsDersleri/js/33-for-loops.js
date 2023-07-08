b87ac8c05b6737f3322c3733de697b9e0c65eb0b
Muzaffer Talha Candan <muzaffertcandan@gmail.com>
2023-07-08 16:27:23 +0300

    33 tamamlandı

---
+++ jsDersleri/js/33-for-loops.js
@@ -0,0 +1,38 @@
+//for loop
+
+// let users = ["muzaffer", "furkan" , "emir", "yusuf"] 
+// // for(başlangıç ataması ; koşul ; arttırım ifadesi)
+// // for (let index = 0; index < users.length; index++){
+// //     console.log(index);
+// // }
+
+// let index = 0;
+
+// // for(; index < users.length; index++){
+// //     console.log(index);
+// // }
+
+// const USER_LİST_DOM = document.querySelector("#user-list")
+
+// for(index = 0; index < users.length; index++){
+//     const liDom = document.createElement("li")
+//     liDom.innerHTML = users[index]
+//     USER_LİST_DOM.appendChild(liDom)
+// }
+let s = "javascriptloops"
+let firstArray = s.split("")
+console.log("first array",firstArray);
+
+
+// let d1 = firstArray.pop("a")
+// console.log("d1",d1);
+function mainfunc(){
+
+}
+
+function vowelsAndConsonants(s) {
+    for(let i =0; i < s.length;i++){
+        console.log(typeof(s))
+    }
+}
+
